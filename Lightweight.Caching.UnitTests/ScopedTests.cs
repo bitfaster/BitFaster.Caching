5de083a34205714220e1d38b5e70d920d3f9fbca
alexpeck <alexpeck@microsoft.com>
2020-06-12 23:39:52 -0700

    formatting

--- Lightweight.Caching.UnitTests/ScopedTests.cs
+++ Lightweight.Caching.UnitTests/ScopedTests.cs
@@ -6,59 +6,58 @@ using Xunit;
 
 namespace Lightweight.Caching.UnitTests
 {
-    public class ScopedDisposableTests
+    public class ScopedTests
     {
-		[Fact]
-		public void WhenScopeIsCreatedThenScopeDisposedLifetimeDisposesValue()
-		{
-			var disposable = new Disposable();
-			var scope = new Scoped<Disposable>(disposable);
-			var lifetime = scope.CreateLifetime();
+        [Fact]
+        public void WhenScopeIsCreatedThenScopeDisposedLifetimeDisposesValue()
+        {
+            var disposable = new Disposable();
+            var scope = new Scoped<Disposable>(disposable);
+            var lifetime = scope.CreateLifetime();
 
-			scope.Dispose();
-			scope.Dispose(); // validate double dispose is still single ref count
-			disposable.IsDisposed.Should().BeFalse();
+            scope.Dispose();
+            scope.Dispose(); // validate double dispose is still single ref count
+            disposable.IsDisposed.Should().BeFalse();
 
-			lifetime.Dispose();
-			disposable.IsDisposed.Should().BeTrue();
-		}
+            lifetime.Dispose();
+            disposable.IsDisposed.Should().BeTrue();
+        }
 
-		[Fact]
-		public void WhenScopeIsCreatedThenLifetimeDisposedScopeDisposesValue()
-		{
-			var disposable = new Disposable();
-			var scope = new Scoped<Disposable>(disposable);
-			var lifetime = scope.CreateLifetime();
+        [Fact]
+        public void WhenScopeIsCreatedThenLifetimeDisposedScopeDisposesValue()
+        {
+            var disposable = new Disposable();
+            var scope = new Scoped<Disposable>(disposable);
+            var lifetime = scope.CreateLifetime();
 
-			lifetime.Dispose();
-			lifetime.Dispose(); // validate double dispose is still single ref count
+            lifetime.Dispose();
+            lifetime.Dispose(); // validate double dispose is still single ref count
 
-			disposable.IsDisposed.Should().BeFalse();
+            disposable.IsDisposed.Should().BeFalse();
 
-			scope.Dispose();
-			disposable.IsDisposed.Should().BeTrue();
-		}
+            scope.Dispose();
+            disposable.IsDisposed.Should().BeTrue();
+        }
 
-		[Fact]
-		public void WhenScopeIsDisposedCreateScopeThrows()
-		{
-			var disposable = new Disposable();
-			var scope = new Scoped<Disposable>(disposable);
-			scope.Dispose();
+        [Fact]
+        public void WhenScopeIsDisposedCreateScopeThrows()
+        {
+            var disposable = new Disposable();
+            var scope = new Scoped<Disposable>(disposable);
+            scope.Dispose();
 
-			scope.Invoking(s => s.CreateLifetime()).Should().Throw<ObjectDisposedException>();
-		}
+            scope.Invoking(s => s.CreateLifetime()).Should().Throw<ObjectDisposedException>();
+        }
 
         private class Disposable : IDisposable
         {
-			public bool IsDisposed { get; set; }
+            public bool IsDisposed { get; set; }
 
             public void Dispose()
             {
-				this.IsDisposed.Should().BeFalse();
-
-				IsDisposed = true;
-			}
+                this.IsDisposed.Should().BeFalse();
+                IsDisposed = true;
+            }
         }
     }
 }
