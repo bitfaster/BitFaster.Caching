e55da66456c713e86205eada26afab5be2460c4f
alexpeck <alexpeck@microsoft.com>
2020-06-14 16:23:24 -0700

    extend singletoncache

---
+++ BitFaster.Caching/SingletonCacheExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitFaster.Caching
+{
+    public static class SingletonCacheExtensions
+    {
+        public static SingletonCache<TKey, TValue>.Handle Acquire<TKey, TValue>(this SingletonCache<TKey, TValue> cache, TKey key) 
+            where TValue : new()
+        {
+            return cache.Acquire(key, _ => new TValue());
+        }
+    }
+}
