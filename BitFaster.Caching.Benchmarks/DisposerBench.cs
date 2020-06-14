2b194e5453060c71e445a02d5bd8602f336ee5f3
alexpeck <alexpeck@microsoft.com>
2020-06-13 20:08:47 -0700

    disposer

---
+++ BitFaster.Caching.Benchmarks/DisposerBench.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.Diagnostics.Runtime.Interop;
+
+namespace BitFaster.Caching.Benchmarks
+{
+    // Is it possible to write a class to eliminate the dispose code for types that are not IDisposable?
+    // https://github.com/dotnet/runtime/issues/4920
+    public class DisposerBench
+    {
+        private Disposer<NotDisposable> notDisposableDisposer = new Disposer<NotDisposable>();
+        private Disposer<Disposable> disposableDisposer = new Disposer<Disposable>();
+
+        [Benchmark(Baseline = true)]
+        public void NotDisposable()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                NotDisposable notDisposable = new NotDisposable();
+                notDisposableDisposer.Dispose(notDisposable); 
+            }
+        }
+
+        [Benchmark()]
+        public void Disposable()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Disposable disposable = new Disposable();
+                disposableDisposer.Dispose(disposable); 
+            }
+        }
+
+    }
+
+    public struct Disposer<T>
+    {
+        public void Dispose(T value)
+        {
+            if (typeof(T) is IDisposable)
+            {
+                ((IDisposable)value).Dispose();
+            }
+        }
+    }
+
+    public class NotDisposable
+    { }
+
+    public class Disposable : IDisposable
+    {
+        public void Dispose()
+        {
+        }
+    }
+}
--- BitFaster.Caching.Benchmarks/Program.cs
+++ BitFaster.Caching.Benchmarks/Program.cs
@@ -15,7 +15,7 @@ namespace BitFaster.Caching.Benchmarks
         static void Main(string[] args)
         {
             var summary = BenchmarkRunner
-                .Run<MissHitHitRemove>(ManualConfig.Create(DefaultConfig.Instance)
+                .Run<DisposerBench>(ManualConfig.Create(DefaultConfig.Instance)
                 .AddJob(Job.RyuJitX64));
         }
     }
