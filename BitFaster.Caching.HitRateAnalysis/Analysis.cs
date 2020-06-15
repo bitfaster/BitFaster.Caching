37749c253129fdbf4031268e9c98ad076d21d56d
Alex Peck <amwpeck@gmail.com>
2020-06-14 23:17:45 -0700

    compare hit rate (#29)
    * compare hit rate
    
    * cleanup

---
+++ BitFaster.Caching.HitRateAnalysis/Analysis.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitFaster.Caching.HitRateAnalysis
+{
+    public class Analysis
+    {
+        public int N { get; set; }
+
+        public double s { get; set; }
+
+        public int Samples { get; set; }
+
+        public double CacheSizePercent { get; set; }
+
+        public void WriteSummaryToConsole()
+        {
+            Console.WriteLine($"Analyzing with N={N}, s={s}, Samples={Samples}, Cache Size ={CacheSizePercent*100.0}%");
+        }
+    }
+}
