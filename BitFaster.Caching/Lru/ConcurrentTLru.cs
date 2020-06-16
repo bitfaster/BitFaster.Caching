237b0dd90911b5115a7efa48efbb3f33ea9ebf8d
Alex Peck <amwpeck@gmail.com>
2020-06-15 19:24:59 -0700

    more ctor (#34)

--- BitFaster.Caching/Lru/ConcurrentTLru.cs
+++ BitFaster.Caching/Lru/ConcurrentTLru.cs
@@ -8,6 +8,11 @@ namespace BitFaster.Caching.Lru
 {
     public sealed class ConcurrentTLru<K, V> : TemplateConcurrentLru<K, V, TimeStampedLruItem<K, V>, TLruDateTimePolicy<K, V>, HitCounter>
     {
+        public ConcurrentTLru(int capacity)
+            : base(Defaults.ConcurrencyLevel, capacity, EqualityComparer<K>.Default, new TLruDateTimePolicy<K, V>(), new HitCounter())
+        { 
+        }
+
         public ConcurrentTLru(int concurrencyLevel, int capacity, IEqualityComparer<K> comparer, TimeSpan timeToLive)
             : base(concurrencyLevel, capacity, comparer, new TLruDateTimePolicy<K, V>(timeToLive), new HitCounter())
         {
--- BitFaster.Caching/Lru/FastConcurrentTLru.cs
+++ BitFaster.Caching/Lru/FastConcurrentTLru.cs
@@ -6,6 +6,11 @@ namespace BitFaster.Caching.Lru
 {
     public sealed class FastConcurrentTLru<K, V> : TemplateConcurrentLru<K, V, TickCountLruItem<K, V>, TLruTicksPolicy<K, V>, NullHitCounter>
     {
+        public FastConcurrentTLru(int capacity)
+            : base(Defaults.ConcurrencyLevel, capacity, EqualityComparer<K>.Default, new TLruTicksPolicy<K, V>(), new NullHitCounter())
+        {
+        }
+
         public FastConcurrentTLru(int concurrencyLevel, int capacity, IEqualityComparer<K> comparer, TimeSpan timeToLive)
             : base(concurrencyLevel, capacity, comparer, new TLruTicksPolicy<K, V>(timeToLive), new NullHitCounter())
         {
