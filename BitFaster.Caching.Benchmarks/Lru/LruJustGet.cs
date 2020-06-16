3ad64ca370df040ee938085e28b6af257639f562
alexpeck <alexpeck@microsoft.com>
2020-06-16 14:13:30 -0700

    add ext cache

--- BitFaster.Caching.Benchmarks/Lru/LruJustGet.cs
+++ BitFaster.Caching.Benchmarks/Lru/LruJustGet.cs
@@ -1,11 +1,12 @@
 ﻿using BenchmarkDotNet.Attributes;
 using BitFaster.Caching;
+using BitFaster.Caching.Benchmarks.Lru;
 using BitFaster.Caching.Lru;
+using Microsoft.Extensions.Caching.Memory;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Caching;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -23,12 +24,16 @@ namespace BitFaster.Caching.Benchmarks
         private static readonly FastConcurrentTLru<int, int> fastConcurrentTLru = new FastConcurrentTLru<int, int>(8, 9, EqualityComparer<int>.Default, TimeSpan.FromMinutes(1));
 
         private static readonly int key = 1;
-        private static MemoryCache memoryCache = MemoryCache.Default;
+        private static System.Runtime.Caching.MemoryCache memoryCache = System.Runtime.Caching.MemoryCache.Default;
+
+        Microsoft.Extensions.Caching.Memory.MemoryCache exMemoryCache 
+            = new Microsoft.Extensions.Caching.Memory.MemoryCache(new MemoryCacheOptionsAccessor());
 
         [GlobalSetup]
         public void GlobalSetup()
         {
-            memoryCache.Set(key.ToString(), "test", new CacheItemPolicy());
+            memoryCache.Set(key.ToString(), "test", new System.Runtime.Caching.CacheItemPolicy());
+            exMemoryCache.Set(key, "test");
         }
 
         [Benchmark(Baseline = true)]
@@ -77,6 +82,21 @@ namespace BitFaster.Caching.Benchmarks
         public void MemoryCacheGetStringKey()
         {
             memoryCache.Get("1");
+        }
+
+        [Benchmark()]
+        public void ExMemoryCacheGetStringKey()
+        {
+            exMemoryCache.Get(1);
+        }
+
+        public class MemoryCacheOptionsAccessor
+            : Microsoft.Extensions.Options.IOptions<MemoryCacheOptions>
+        {
+            private readonly MemoryCacheOptions options = new MemoryCacheOptions();
+
+            public MemoryCacheOptions Value => this.options;
+
         }
     }
 }
