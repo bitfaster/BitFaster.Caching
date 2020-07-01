46ac41365d09d7b5ea8c5076b386503fd958eddf
alexpeck <alexpeck@microsoft.com>
2020-06-30 20:36:58 -0700

    throughput test

---
+++ BitFaster.Caching.ThroughputAnalysis/ThreadSafeRandom.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitFaster.Caching.ThroughputAnalysis
+{
+    public static class ThreadSafeRandom
+    {
+        [ThreadStatic]
+        private static Random _local;
+
+        private static Random _global = new Random();
+
+        public static int Next(int max)
+        {
+            Random inst = _local;
+            if (inst == null)
+            {
+                int seed;
+                lock (_global) seed = _global.Next();
+                _local = inst = new Random(seed);
+            }
+            return inst.Next(max);
+        }
+    }
+}
