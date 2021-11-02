d9a4aaf0896a6a5bf6c109021e8128ba4ffb5fde
Alex Peck <alexpeck@microsoft.com>
2021-11-01 18:44:21 -0700

    comment

--- BitFaster.Caching/Disposer.cs
+++ BitFaster.Caching/Disposer.cs
@@ -7,8 +7,16 @@ using System.Threading.Tasks;
 
 namespace BitFaster.Caching
 {
+    /// <summary>
+    /// A generic wrapper for object disposal. Enables JIT to inline/remove object disposal if statement reducing code size.
+    /// </summary>
+    /// <typeparam name="T">The type of object to dispose</typeparam>
     public static class Disposer<T>
     { 
+        /// <summary>
+        /// Dispose value if it implements the IDisposable interface.
+        /// </summary>
+        /// <param name="value">The value to dispose.</param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void Dispose(T value)
         {
