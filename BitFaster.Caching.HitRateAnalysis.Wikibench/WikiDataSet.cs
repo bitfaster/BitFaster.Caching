99364434c8fde6714a322865b6aba238d7140dd9
Alex Peck <alexpeck@microsoft.com>
2022-07-10 21:54:21 -0700

    basic

---
+++ BitFaster.Caching.HitRateAnalysis.Wikibench/WikiDataSet.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitFaster.Caching.HitRateAnalysis.Wikibench
+{
+    public class WikiDataSet
+    {
+        List<WikiBenchFile> files = new List<WikiBenchFile>();
+
+        public WikiDataSet(IEnumerable<string> urls)
+        {
+            int n = 1;
+            foreach (string url in urls)
+            {
+                files.Add(new WikiBenchFile(n++, new Uri(url)));
+            }
+        }
+
+        public async Task Download()
+        {
+            var tasks = new List<Task>();
+
+            foreach (var f in files)
+            {
+                tasks.Add(Task.Run(async () => await f.DownloadIfNotExists()));
+            }
+
+            await Task.WhenAll(tasks);
+        }
+
+        public IEnumerable<Uri> EnumerateUris()
+        {
+            foreach (var f in files)
+            {
+                foreach (var uri in f.EnumerateUris())
+                {
+                    yield return uri;
+                }
+            }
+        }
+    }
+}
