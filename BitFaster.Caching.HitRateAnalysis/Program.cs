11ddf6503fec0ad22ddff7f6e89d4a427046c766
Alex Peck <alexpeck@microsoft.com>
2022-07-14 20:02:27 -0700

    test arc data

--- BitFaster.Caching.HitRateAnalysis/Program.cs
+++ BitFaster.Caching.HitRateAnalysis/Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Threading.Tasks;
+using BitFaster.Caching.HitRateAnalysis.Arc;
 
 var menu = new EasyConsole.Menu()
     .Add("Zipf", () => BitFaster.Caching.HitRateAnalysis.Zipfian.Runner.Run())
     .Add("Wikibench", () => BitFaster.Caching.HitRateAnalysis.Wikibench.Runner.Run().Wait())
-    .Add("Glimpse", () => BitFaster.Caching.HitRateAnalysis.Glimpse.Runner.Run().Wait());
+    .Add("Glimpse", () => BitFaster.Caching.HitRateAnalysis.Glimpse.Runner.Run().Wait())
+    .Add("Arc Database", () => new BitFaster.Caching.HitRateAnalysis.Arc.Runner(RunnerConfig.Database).Run().Wait())
+    .Add("Arc Search", () => new BitFaster.Caching.HitRateAnalysis.Arc.Runner(RunnerConfig.Search).Run().Wait())
+    .Add("Arc OLTP", () => new BitFaster.Caching.HitRateAnalysis.Arc.Runner(RunnerConfig.Oltp).Run().Wait());
 
 menu.Display();
