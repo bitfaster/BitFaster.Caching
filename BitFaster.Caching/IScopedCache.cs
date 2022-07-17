9d4729580dc440b883c5ad734432c04b5a587d3b
Alex Peck <alexpeck@microsoft.com>
2022-07-16 19:47:21 -0700

    scopedec

---
+++ BitFaster.Caching/IScopedCache.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitFaster.Caching
+{
+    public interface IScopedCache<K, V> where V : IDisposable
+    {
+        /// <summary>
+        /// Gets the total number of items that can be stored in the cache.
+        /// </summary>
+        int Capacity { get; }
+
+        /// <summary>
+        /// Gets the number of items currently held in the cache.
+        /// </summary>
+        int Count { get; }
+
+        /// <summary>
+        /// Attempts to get the value associated with the specified key from the cache.
+        /// </summary>
+        /// <param name="key">The key of the value to get.</param>
+        /// <param name="lifetime">When this method returns, contains the object from the cache that has the specified key, or the default value of the type if the operation failed.</param>
+        /// <returns>true if the key was found in the cache; otherwise, false.</returns>
+        bool TryGet(K key, out Lifetime<V> lifetime);
+
+        /// <summary>
+        /// Adds a key/value pair to the cache if the key does not already exist. Returns the new value, or the 
+        /// existing value if the key already exists.
+        /// </summary>
+        /// <param name="key">The key of the element to add.</param>
+        /// <param name="valueFactory">The factory function used to generate a value for the key.</param>
+        /// <returns>The value for the key. This will be either the existing value for the key if the key is already 
+        /// in the cache, or the new value if the key was not in the dictionary.</returns>
+        Lifetime<V> GetOrAdd(K key, Func<K, Scoped<V>> valueFactory);
+
+        /// <summary>
+        /// Adds a key/value pair to the cache if the key does not already exist. Returns the new value, or the 
+        /// existing value if the key already exists.
+        /// </summary>
+        /// <param name="key">The key of the element to add.</param>
+        /// <param name="valueFactory">The factory function used to asynchronously generate a value for the key.</param>
+        /// <returns>A task that represents the asynchronous GetOrAdd operation.</returns>
+        Task<Lifetime<V>> GetOrAddAsync(K key, Func<K, Task<Scoped<V>>> valueFactory);
+
+        /// <summary>
+        /// Attempts to remove the value that has the specified key.
+        /// </summary>
+        /// <param name="key">The key of the element to remove.</param>
+        /// <returns>true if the object was removed successfully; otherwise, false.</returns>
+        bool TryRemove(K key);
+
+        /// <summary>
+        /// Attempts to update the value that has the specified key.
+        /// </summary>
+        /// <param name="key">The key of the element to update.</param>
+        /// <param name="value">The new value.</param>
+        /// <returns>true if the object was updated successfully; otherwise, false.</returns>
+        bool TryUpdate(K key, V value);
+
+        /// <summary>
+        /// Adds a key/value pair to the cache if the key does not already exist, or updates a key/value pair if the 
+        /// key already exists.
+        /// </summary>
+        /// <param name="key">The key of the element to update.</param>
+        /// <param name="value">The new value.</param>
+        void AddOrUpdate(K key, V value);
+
+        /// <summary>
+        /// Removes all keys and values from the cache.
+        /// </summary>
+        void Clear();
+
+        /// <summary>
+        /// Trim the specified number of items from the cache.
+        /// </summary>
+        /// <param name="itemCount">The number of items to remove.</param>
+        void Trim(int itemCount);
+    }
+}
---
+++ BitFaster.Caching/ScopedCache.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BitFaster.Caching
+{
+    // simple decorator, compatible with all ICache
+    public class ScopedCache<K, V> : IScopedCache<K, V> where V : IDisposable
+    {
+        private readonly ICache<K, Scoped<V>> cache;
+
+        public ScopedCache(ICache<K, Scoped<V>> cache)
+        {
+            this.cache = cache;
+        }
+
+        public int Capacity => this.cache.Capacity;
+
+        public int Count => this.cache.Count;
+
+        public void AddOrUpdate(K key, V value)
+        {
+            this.cache.AddOrUpdate(key, new Scoped<V>(value));
+        }
+
+        public void Clear()
+        {
+            this.cache.Clear();
+        }
+
+        public Lifetime<V> GetOrAdd(K key, Func<K, Scoped<V>> valueFactory)
+        {
+            var spinwait = new SpinWait();
+            while (true)
+            {
+                var scope = cache.GetOrAdd(key, k => valueFactory(k));
+
+                if (scope.TryCreateLifetime(out var lifetime))
+                {
+                    return lifetime;
+                }
+
+                spinwait.SpinOnce();
+            }
+        }
+
+        public async Task<Lifetime<V>> GetOrAddAsync(K key, Func<K, Task<Scoped<V>>> valueFactory)
+        {
+            var spinwait = new SpinWait();
+            while (true)
+            {
+                var scope = await cache.GetOrAddAsync(key, valueFactory);
+
+                if (scope.TryCreateLifetime(out var lifetime))
+                {
+                    return lifetime;
+                }
+
+                spinwait.SpinOnce();
+            }
+        }
+
+        public void Trim(int itemCount)
+        {
+            this.cache.Trim(itemCount);
+        }
+
+        public bool TryGet(K key, out Lifetime<V> lifetime)
+        {
+            if (this.cache.TryGet(key, out var scope))
+            {
+                if (scope.TryCreateLifetime(out lifetime))
+                {
+                    return true;
+                }
+            }
+
+            lifetime = default;
+            return false;
+        }
+
+        public bool TryRemove(K key)
+        {
+            return this.cache.TryRemove(key);
+        }
+
+        public bool TryUpdate(K key, V value)
+        {
+            return this.cache.TryUpdate(key, new Scoped<V>(value));
+        }
+    }
+}
