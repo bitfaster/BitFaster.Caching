92ab5785da004e59f6228ba008f8d8cc01985bd3
Alex Peck <alexpeck@microsoft.com>
2022-07-23 18:40:02 -0700

    sac tests

--- BitFaster.Caching.UnitTests/ScopedAsyncCacheTests.cs
+++ BitFaster.Caching.UnitTests/ScopedAsyncCacheTests.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BitFaster.Caching.Lru;
+using FluentAssertions;
+using Xunit;
 
 namespace BitFaster.Caching.UnitTests
 {
@@ -12,6 +14,45 @@ namespace BitFaster.Caching.UnitTests
         public ScopedAsyncCacheTests() 
             : base(new ScopedAsyncCache<int, Disposable>(new ConcurrentLru<int, Scoped<Disposable>>(capacity)))
         {
+        }
+
+        [Fact]
+        public void WhenInnerCacheIsNullCtorThrows()
+        {
+            Action constructor = () => { var x = new ScopedAsyncCache<int, Disposable>(null); };
+
+            constructor.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public async Task WhenScopeIsDisposedTryGetReturnsFalse()
+        {
+            var scope = new Scoped<Disposable>(new Disposable());
+
+            await this.cache.ScopedGetOrAddAsync(1, k => Task.FromResult(scope));
+
+            scope.Dispose();
+
+            this.cache.ScopedTryGet(1, out var lifetime).Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task WhenKeyDoesNotExistGetOrAddAsyncAddsValue()
+        {
+            await this.cache.ScopedGetOrAddAsync(1, k => Task.FromResult(new Scoped<Disposable>(new Disposable())));
+
+            this.cache.ScopedTryGet(1, out var lifetime).Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task GetOrAddAsyncDisposedScopeThrows()
+        {
+            var scope = new Scoped<Disposable>(new Disposable());
+            scope.Dispose();
+
+            Func<Task> getOrAdd = async () => { await this.cache.ScopedGetOrAddAsync(1, k => Task.FromResult(scope)); };
+
+            await getOrAdd.Should().ThrowAsync<InvalidOperationException>();
         }
     }
 }
