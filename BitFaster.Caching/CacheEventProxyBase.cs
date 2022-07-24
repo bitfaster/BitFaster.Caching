cf9971b691e136659d6cd1f4023901a42cb2ad1d
Alex Peck <alexpeck@microsoft.com>
2022-07-23 19:48:43 -0700

    clean

--- BitFaster.Caching/CacheEventProxyBase.cs
+++ BitFaster.Caching/CacheEventProxyBase.cs
@@ -10,13 +10,7 @@ namespace BitFaster.Caching
     {
         private readonly ICacheEvents<K, TInner> events;
 
-/* Unmerged change from project 'BitFaster.Caching (netcoreapp3.1)'
-Before:
-        private event EventHandler<Lru.ItemRemovedEventArgs<K, TOuter>> itemRemovedProxy;
-After:
         private event EventHandler<ItemRemovedEventArgs<K, TOuter>> itemRemovedProxy;
-*/
-        private event EventHandler<Caching.ItemRemovedEventArgs<K, TOuter>> itemRemovedProxy;
 
         public CacheEventProxyBase(ICacheEvents<K, TInner> events)
         {
@@ -25,40 +19,19 @@ After:
 
         public bool IsEnabled => this.events.IsEnabled;
 
-
-/* Unmerged change from project 'BitFaster.Caching (netcoreapp3.1)'
-Before:
-        public event EventHandler<Lru.ItemRemovedEventArgs<K, TOuter>> ItemRemoved
-After:
         public event EventHandler<ItemRemovedEventArgs<K, TOuter>> ItemRemoved
-*/
-        public event EventHandler<Caching.ItemRemovedEventArgs<K, TOuter>> ItemRemoved
         {
             add { this.Register(value); }
             remove { this.UnRegister(value); }
         }
 
-
-/* Unmerged change from project 'BitFaster.Caching (netcoreapp3.1)'
-Before:
-        private void Register(EventHandler<Lru.ItemRemovedEventArgs<K, TOuter>> value)
-After:
         private void Register(EventHandler<ItemRemovedEventArgs<K, TOuter>> value)
-*/
-        private void Register(EventHandler<Caching.ItemRemovedEventArgs<K, TOuter>> value)
         {
             itemRemovedProxy += value;
             events.ItemRemoved += OnItemRemoved;
         }
 
-
-/* Unmerged change from project 'BitFaster.Caching (netcoreapp3.1)'
-Before:
-        private void UnRegister(EventHandler<Lru.ItemRemovedEventArgs<K, TOuter>> value)
-After:
         private void UnRegister(EventHandler<ItemRemovedEventArgs<K, TOuter>> value)
-*/
-        private void UnRegister(EventHandler<Caching.ItemRemovedEventArgs<K, TOuter>> value)
         {
             this.itemRemovedProxy -= value;
 
@@ -68,14 +41,7 @@ After:
             }
         }
 
-
-/* Unmerged change from project 'BitFaster.Caching (netcoreapp3.1)'
-Before:
-        private void OnItemRemoved(object sender, Lru.ItemRemovedEventArgs<K, TInner> args)
-After:
         private void OnItemRemoved(object sender, ItemRemovedEventArgs<K, TInner> args)
-*/
-        private void OnItemRemoved(object sender, Caching.ItemRemovedEventArgs<K, TInner> args)
         {
             itemRemovedProxy(sender, TranslateOnRemoved(args));
         }
