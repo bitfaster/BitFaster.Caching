008993c038c3c294bd82d50254d2ec8292af8c1e
Alex Peck <alexpeck@microsoft.com>
2022-07-26 11:42:17 -0700

    ocpr

---
+++ BitFaster.Caching/CachePolicy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitFaster.Caching
+{
+    public class CachePolicy
+    {
+        public CachePolicy(IBoundedPolicy eviction, ITimePolicy expireAfterWrite)
+        {
+            this.Eviction = eviction;
+            this.ExpireAfterWrite = expireAfterWrite;
+        }
+
+        public IBoundedPolicy Eviction { get; }
+
+        public ITimePolicy ExpireAfterWrite { get; }
+    }
+}
