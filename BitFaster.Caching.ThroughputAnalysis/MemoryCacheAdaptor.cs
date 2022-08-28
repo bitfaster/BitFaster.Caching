164b9b180d0c39d48f392dc960c11dab0c8bdeeb
Alex Peck <amwpeck@gmail.com>
2022-08-27 23:10:36 -0700

    memorycache (#216)
    * memorycache
    
    * size
    
    * format

---
+++ BitFaster.Caching.ThroughputAnalysis/MemoryCacheAdaptor.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace BitFaster.Caching.ThroughputAnalysis
+{
+    public class MemoryCacheAdaptor<K, V> : ICache<K, V>
+    {
+        MemoryCacheOptionsAccessor accessor;
+        MemoryCache exMemoryCache;
+
+        public MemoryCacheAdaptor(int capacity)
+        {
+            accessor = new MemoryCacheOptionsAccessor();
+            accessor.Value.SizeLimit = capacity;
+
+            exMemoryCache = new MemoryCache(accessor);
+        }
+
+        public int Count => throw new NotImplementedException();
+
+        public Optional<ICacheMetrics> Metrics => throw new NotImplementedException();
+
+        public Optional<ICacheEvents<K, V>> Events => throw new NotImplementedException();
+
+        public CachePolicy Policy => throw new NotImplementedException();
+
+        public ICollection<K> Keys => throw new NotImplementedException();
+
+        public void AddOrUpdate(K key, V value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<KeyValuePair<K, V>> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public V GetOrAdd(K key, Func<K, V> valueFactory)
+        {
+
+            if (!exMemoryCache.TryGetValue(key, out object result))
+            {
+                using ICacheEntry entry = exMemoryCache.CreateEntry(key);
+
+                result = valueFactory(key);
+                entry.Value = result;
+                entry.SetSize(1);
+            }
+
+            return (V)result;
+        }
+
+        public bool TryGet(K key, out V value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryRemove(K key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryUpdate(K key, V value)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class MemoryCacheOptionsAccessor
+        : Microsoft.Extensions.Options.IOptions<MemoryCacheOptions>
+    {
+        private readonly MemoryCacheOptions options = new MemoryCacheOptions();
+
+        public MemoryCacheOptions Value => this.options;
+
+    }
+}
