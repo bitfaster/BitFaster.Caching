d70657e63aba338922bdefdf541e7cac164d23de
Alex Peck <alexpeck@microsoft.com>
2022-09-08 17:44:18 -0700

    docs

--- BitFaster.Caching/Lfu/Builder/AtomicScopedAsyncConcurrentLfuBuilder.cs
+++ BitFaster.Caching/Lfu/Builder/AtomicScopedAsyncConcurrentLfuBuilder.cs
@@ -1,10 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using BitFaster.Caching.Atomic;
 
 namespace BitFaster.Caching.Lfu.Builder
 {
+    /// <summary>
+    /// A builder for creating a ConcurrentLfu as IScopedAsyncCache with atomic value creation.
+    /// </summary>
+    /// <typeparam name="K">The type of the cache key.</typeparam>
+    /// <typeparam name="V">The type of the cache value.</typeparam>
     public sealed class AtomicScopedAsyncConcurrentLfuBuilder<K, V> : LfuBuilderBase<K, V, AtomicScopedAsyncConcurrentLfuBuilder<K, V>, IScopedAsyncCache<K, V>> where V : IDisposable
     {
         private readonly AsyncConcurrentLfuBuilder<K, ScopedAsyncAtomicFactory<K, V>> inner;
