022affbe6fa50ec7c0c238c4293eb51cd91d6161
Alex Peck <amwpeck@gmail.com>
2022-09-17 14:10:08 -0700

    cache freq (#264)
    * avx
    
    * tests
    
    * cleanup
    
    * format
    
    * revert bench changes
    
    * fix cmpr
    
    * hmin
    
    * mul
    
    * optimized
    
    * cachef
    
    * iter
    
    * cleanup
    
    * detect

--- BitFaster.Caching/Lfu/ConcurrentLfu.cs
+++ BitFaster.Caching/Lfu/ConcurrentLfu.cs
@@ -615,36 +615,67 @@ namespace BitFaster.Caching.Lfu
             return first;
         }
 
-        private void EvictFromMain(LfuNode<K, V> candidate)
+        private struct EvictIterator
         {
-            var victim = this.probationLru.First; // victims are LRU position in probation
+            private CmSketch<K, DetectAvx2> sketch;
+            public LfuNode<K, V> node;
+            public int freq;
+
+            public EvictIterator(CmSketch<K, DetectAvx2> sketch, LfuNode<K, V> node)
+            {
+                this.sketch = sketch;
+                this.node = node;
+                freq = node == null ? -1 : sketch.EstimateFrequency(node.Key);
+            }
+
+            public void Next()
+            {
+                node = node.Next;
+
+                if (node != null)
+                {
+                    freq = sketch.EstimateFrequency(node.Key);
+                }
+            }
+        }
+
+        private void EvictFromMain(LfuNode<K, V> candidateNode)
+        {
+            var victim = new EvictIterator(this.cmSketch, this.probationLru.First); // victims are LRU position in probation
+            var candidate = new EvictIterator(this.cmSketch, candidateNode);
 
             // first pass: admit candidates
             while (this.windowLru.Count + this.probationLru.Count + this.protectedLru.Count > this.Capacity)
             {
                 // bail when we run out of options
-                if (candidate == null | victim == null | victim == candidate)
+                if (candidate.node == null | victim.node == null)
                 {
                     break;
                 }
 
-                // Evict the entry with the lowest frequency
-                if (AdmitCandidate(candidate.Key, victim.Key))
+                if (victim.node == candidate.node)
                 {
-                    var evictee = victim;
+                    Evict(candidate.node);
+                    break;
+                }
+
+                // Evict the entry with the lowest frequency
+                if (candidate.freq > victim.freq)
+                {
+                    var evictee = victim.node;
 
                     // victim is initialized to first, and iterates forwards
-                    victim = victim.Next;
-                    candidate = candidate.Next;
+                    victim.Next();
+                    candidate.Next();
 
                     Evict(evictee);
                 }
                 else
                 {
-                    var evictee = candidate;
+                    var evictee = candidate.node;
 
-                    // candidate is initialized to last, and iterates backwards
-                    candidate = candidate.Next;
+                    // candidate is initialized to first cand, and iterates forwards
+                    candidate.Next();
 
                     Evict(evictee);
                 }
@@ -653,16 +684,16 @@ namespace BitFaster.Caching.Lfu
             // 2nd pass: remove probation items in LRU order, evict lowest frequency
             while (this.windowLru.Count + this.probationLru.Count + this.protectedLru.Count > this.Capacity)
             {
-                victim = this.probationLru.First;
-                var victim2 = victim.Next;
+                var victim1 = this.probationLru.First;
+                var victim2 = victim1.Next;
 
-                if (AdmitCandidate(victim.Key, victim2.Key))
+                if (AdmitCandidate(victim1.Key, victim2.Key))
                 {
                     Evict(victim2);
                 }
                 else
                 {
-                    Evict(victim);
+                    Evict(victim1);
                 }
             }
         }
