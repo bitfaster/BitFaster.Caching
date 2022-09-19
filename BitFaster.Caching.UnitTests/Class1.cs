48c6d1e5b2acb905fb5015b9ea98473997a3a20b
Alex Peck <alexpeck@microsoft.com>
2022-09-18 17:32:17 -0700

    test

---
+++ BitFaster.Caching.UnitTests/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BitFaster.Caching.UnitTests
+{
+    public class Class1
+    {
+        [Fact]
+        public void ArmTest()
+        {
+            if (AdvSimd.IsSupported)
+            {
+                throw new Exception("AdvSimd!");
+            }
+        }
+    }
+}
