02e6fb99b16df646cdaf54e785f6bc36536ea6fd
Alex Peck <alexpeck@microsoft.com>
2022-10-08 19:20:47 -0700

    align

--- BitFaster.Caching.Benchmarks/Lfu/SketchReset.cs
+++ BitFaster.Caching.Benchmarks/Lfu/SketchReset.cs
@@ -2,6 +2,7 @@
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Jobs;
 using BitFaster.Caching.Lfu;
+using Microsoft.Diagnostics.Tracing.StackSources;
 using System.Collections.Generic;
 using System.Runtime.Intrinsics;
 using System.Runtime.Intrinsics.X86;
@@ -17,11 +18,11 @@ namespace BitFaster.Caching.Benchmarks.Lfu
 
         long[] table;
 
-        [Params(4, 128, 8192, 1048576)]
+        [Params(8192, 1048576)]
         public int Size { get; set; }
 
         [GlobalSetup]
-        public void Setup()
+        public unsafe void Setup()
         {
             table = new long[Size];
         }
@@ -39,63 +40,63 @@ namespace BitFaster.Caching.Benchmarks.Lfu
             return count;
         }
 
-        [Benchmark()]
-        public int Reset2()
-        {
-            int count0 = 0;
-            int count1 = 0;
+        //[Benchmark()]
+        //public int Reset2()
+        //{
+        //    int count0 = 0;
+        //    int count1 = 0;
 
-            for (int i = 0; i < table.Length; i += 2)
-            {
-                count0 += BitOps.BitCount(table[i] & OneMask);
-                count1 += BitOps.BitCount(table[i + 1] & OneMask);
+        //    for (int i = 0; i < table.Length; i += 2)
+        //    {
+        //        count0 += BitOps.BitCount(table[i] & OneMask);
+        //        count1 += BitOps.BitCount(table[i + 1] & OneMask);
 
-                table[i] = (long)((ulong)table[i] >> 1) & ResetMask;
-                table[i + 1] = (long)((ulong)table[i + 1] >> 1) & ResetMask;
-            }
+        //        table[i] = (long)((ulong)table[i] >> 1) & ResetMask;
+        //        table[i + 1] = (long)((ulong)table[i + 1] >> 1) & ResetMask;
+        //    }
 
-            return count0 + count1;
-        }
+        //    return count0 + count1;
+        //}
 
-        [Benchmark()]
-        public int Reset4()
-        {
-            int count0 = 0;
-            int count1 = 0;
-            int count2 = 0;
-            int count3 = 0;
+        //[Benchmark()]
+        //public int Reset4()
+        //{
+        //    int count0 = 0;
+        //    int count1 = 0;
+        //    int count2 = 0;
+        //    int count3 = 0;
 
-            for (int i = 0; i < table.Length; i += 4)
-            {
-                count0 += BitOps.BitCount(table[i] & OneMask);
-                count1 += BitOps.BitCount(table[i + 1] & OneMask);
-                count2 += BitOps.BitCount(table[i + 2] & OneMask);
-                count3 += BitOps.BitCount(table[i + 3] & OneMask);
+        //    for (int i = 0; i < table.Length; i += 4)
+        //    {
+        //        count0 += BitOps.BitCount(table[i] & OneMask);
+        //        count1 += BitOps.BitCount(table[i + 1] & OneMask);
+        //        count2 += BitOps.BitCount(table[i + 2] & OneMask);
+        //        count3 += BitOps.BitCount(table[i + 3] & OneMask);
 
-                table[i] = (long)((ulong)table[i] >> 1) & ResetMask;
-                table[i + 1] = (long)((ulong)table[i + 1] >> 1) & ResetMask;
-                table[i + 2] = (long)((ulong)table[i + 2] >> 1) & ResetMask;
-                table[i + 3] = (long)((ulong)table[i + 3] >> 1) & ResetMask;
-            }
+        //        table[i] = (long)((ulong)table[i] >> 1) & ResetMask;
+        //        table[i + 1] = (long)((ulong)table[i + 1] >> 1) & ResetMask;
+        //        table[i + 2] = (long)((ulong)table[i + 2] >> 1) & ResetMask;
+        //        table[i + 3] = (long)((ulong)table[i + 3] >> 1) & ResetMask;
+        //    }
 
-            return (count0 + count1) + (count2 + count3);
-        }
+        //    return (count0 + count1) + (count2 + count3);
+        //}
 
-        [Benchmark()]
-        public int Reset4NoPopcount()
-        {
-            for (int i = 0; i < table.Length; i += 4)
-            {
-                table[i] = (long)((ulong)table[i] >> 1) & ResetMask;
-                table[i + 1] = (long)((ulong)table[i + 1] >> 1) & ResetMask;
-                table[i + 2] = (long)((ulong)table[i + 2] >> 1) & ResetMask;
-                table[i + 3] = (long)((ulong)table[i + 3] >> 1) & ResetMask;
-            }
+        //[Benchmark()]
+        //public int Reset4NoPopcount()
+        //{
+        //    for (int i = 0; i < table.Length; i += 4)
+        //    {
+        //        table[i] = (long)((ulong)table[i] >> 1) & ResetMask;
+        //        table[i + 1] = (long)((ulong)table[i + 1] >> 1) & ResetMask;
+        //        table[i + 2] = (long)((ulong)table[i + 2] >> 1) & ResetMask;
+        //        table[i + 3] = (long)((ulong)table[i + 3] >> 1) & ResetMask;
+        //    }
 
-            return 0;
-        }
+        //    return 0;
+        //}
 
-        [Benchmark()]
+        //[Benchmark()]
         public unsafe int ResetAVXNoPopcount()
         {
             var resetMaskVector = Vector256.Create(ResetMask);
@@ -114,34 +115,34 @@ namespace BitFaster.Caching.Benchmarks.Lfu
             return 0;
         }
 
-        [Benchmark()]
-        public unsafe int ResetAVXNoPopcountUnroll2()
-        {
-            if (table.Length < 16)
-            {
-                return ResetAVXNoPopcount();
-            }
+        //[Benchmark()]
+        //public unsafe int ResetAVXNoPopcountUnroll2()
+        //{
+        //    if (table.Length < 16)
+        //    {
+        //        return ResetAVXNoPopcount();
+        //    }
 
-            var resetMaskVector = Vector256.Create(ResetMask);
+        //    var resetMaskVector = Vector256.Create(ResetMask);
 
-            fixed (long* tPtr = &table[0])
-            {
-                for (int i = 0; i < table.Length; i += 8)
-                {
-                    Vector256<long> t1 = Avx2.LoadVector256(tPtr + i).AsInt64();
-                    t1 = Avx2.ShiftRightLogical(t1, 1);
-                    t1 = Avx2.And(t1, resetMaskVector);
-                    Avx2.Store(tPtr + i, t1);
+        //    fixed (long* tPtr = &table[0])
+        //    {
+        //        for (int i = 0; i < table.Length; i += 8)
+        //        {
+        //            Vector256<long> t1 = Avx2.LoadVector256(tPtr + i).AsInt64();
+        //            t1 = Avx2.ShiftRightLogical(t1, 1);
+        //            t1 = Avx2.And(t1, resetMaskVector);
+        //            Avx2.Store(tPtr + i, t1);
 
-                    Vector256<long> t2 = Avx2.LoadVector256(tPtr + i + 4).AsInt64();
-                    t2 = Avx2.ShiftRightLogical(t2, 1);
-                    t2 = Avx2.And(t2, resetMaskVector);
-                    Avx2.Store(tPtr + i + 4, t2);
-                }
-            }
+        //            Vector256<long> t2 = Avx2.LoadVector256(tPtr + i + 4).AsInt64();
+        //            t2 = Avx2.ShiftRightLogical(t2, 1);
+        //            t2 = Avx2.And(t2, resetMaskVector);
+        //            Avx2.Store(tPtr + i + 4, t2);
+        //        }
+        //    }
 
-            return 0;
-        }
+        //    return 0;
+        //}
 
         [Benchmark()]
         public unsafe int ResetAVXNoPopcountUnroll4()
@@ -180,6 +181,61 @@ namespace BitFaster.Caching.Benchmarks.Lfu
             }
 
             return 0;
+        }
+
+        [Benchmark()]
+        public unsafe int ResetAVXAlignedNoPopcountUnroll4()
+        {
+            if (table.Length < 16)
+            {
+                return ResetAVXNoPopcount();
+            }
+
+            var resetMaskVector = Vector256.Create(ResetMask);
+
+            fixed (long* tPtr = &table[0])
+            {
+                long* alignedPtr = tPtr;
+
+                while (((ulong)alignedPtr & 31UL) != 0)
+                {
+                    *alignedPtr = (*alignedPtr >> 1) & ResetMask;
+                    alignedPtr++;
+                }
+
+                int c = table.Length - (int)(alignedPtr - tPtr) -16;
+
+                for (int i = 0; i < c; i += 16)
+                {
+                    Vector256<long> t1 = Avx2.LoadAlignedVector256(alignedPtr + i).AsInt64();
+                    Vector256<long> t2 = Avx2.LoadAlignedVector256(alignedPtr + i + 4).AsInt64();
+                    Vector256<long> t3 = Avx2.LoadAlignedVector256(alignedPtr + i + 8).AsInt64();
+                    Vector256<long> t4 = Avx2.LoadAlignedVector256(alignedPtr + i + 12).AsInt64();
+
+                    t1 = Avx2.ShiftRightLogical(t1, 1);
+                    t2 = Avx2.ShiftRightLogical(t2, 1);
+                    t3 = Avx2.ShiftRightLogical(t3, 1);
+                    t4 = Avx2.ShiftRightLogical(t4, 1);
+
+                    t1 = Avx2.And(t1, resetMaskVector);
+                    t2 = Avx2.And(t2, resetMaskVector);
+                    t3 = Avx2.And(t3, resetMaskVector);
+                    t4 = Avx2.And(t4, resetMaskVector);
+
+                    Avx2.StoreAligned(alignedPtr + i, t1);
+                    Avx2.StoreAligned(alignedPtr + i + 4, t2);
+                    Avx2.StoreAligned(alignedPtr + i + 8, t3);
+                    Avx2.StoreAligned(alignedPtr + i + 12, t4);
+                }
+
+                for (int i = c; i < table.Length; i++)
+                {
+                    *alignedPtr = (*alignedPtr >> 1) & ResetMask;
+                    alignedPtr++;
+                }
+
+                return 0;
+            }
         }
     }
 }
