b2b346f70dbd8a3ee95738a6d8e1c78fc8a11d1e
Alex Peck <alexpeck@microsoft.com>
2022-10-13 21:55:48 -0700

    all params

--- BitFaster.Caching.ThroughputAnalysis/SizeExec.bat
+++ BitFaster.Caching.ThroughputAnalysis/SizeExec.bat
@@ -3,7 +3,20 @@ cls
 @echo off
 set DOTNET_Thread_UseAllCpuGroups=1
 
+call BitFaster.Caching.ThroughputAnalysis.exe 1 100
+call BitFaster.Caching.ThroughputAnalysis.exe 1 10000
+call BitFaster.Caching.ThroughputAnalysis.exe 1 1000000
+call BitFaster.Caching.ThroughputAnalysis.exe 1 10000000
+
 call BitFaster.Caching.ThroughputAnalysis.exe 2 1000
+call BitFaster.Caching.ThroughputAnalysis.exe 2 100000
+call BitFaster.Caching.ThroughputAnalysis.exe 2 10000000
+call BitFaster.Caching.ThroughputAnalysis.exe 2 100000000
+
+call BitFaster.Caching.ThroughputAnalysis.exe 3 100
+call BitFaster.Caching.ThroughputAnalysis.exe 3 10000
+call BitFaster.Caching.ThroughputAnalysis.exe 3 1000000
+call BitFaster.Caching.ThroughputAnalysis.exe 3 10000000
 
 call BitFaster.Caching.ThroughputAnalysis.exe 4 100
 call BitFaster.Caching.ThroughputAnalysis.exe 4 10000
