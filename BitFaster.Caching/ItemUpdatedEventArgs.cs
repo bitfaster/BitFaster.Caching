58a3fdbbe2993f82b76275fd7d1d1edba9c841f3
Alex Peck <alexpeck@microsoft.com>
2022-10-28 18:37:37 -0700

    docs

--- BitFaster.Caching/ItemUpdatedEventArgs.cs
+++ BitFaster.Caching/ItemUpdatedEventArgs.cs
@@ -5,14 +5,14 @@ namespace BitFaster.Caching
     /// <summary>
     /// Provides data for the ItemUpdated event.
     /// </summary>
-    /// <typeparam name="K">The type of the removed item key.</typeparam>
-    /// <typeparam name="V">The type of the removed item value.</typeparam>
+    /// <typeparam name="K">The type of the updated item key.</typeparam>
+    /// <typeparam name="V">The type of the updated item value.</typeparam>
     public class ItemUpdatedEventArgs<K, V> : EventArgs
     {
         /// <summary>
-        /// Initializes a new instance of the ItemRemovedEventArgs class using the specified key, value and reason.
+        /// Initializes a new instance of the ItemUpdatedEventArgs class using the specified key, old value and new value.
         /// </summary>
-        /// <param name="key">The key of the item that was removed from the cache.</param>
+        /// <param name="key">The key of the item that was updated.</param>
         /// <param name="oldValue">The old cache value.</param>
         /// <param name="newValue">The new cache value.</param>
         public ItemUpdatedEventArgs(K key, V oldValue, V newValue)
@@ -23,17 +23,17 @@ namespace BitFaster.Caching
         }
 
         /// <summary>
-        /// Gets the key of the item that was removed from the cache.
+        /// Gets the key of the item that was updated.
         /// </summary>
         public K Key { get; }
 
         /// <summary>
-        /// Gets the old value of the item that was removed from the cache.
+        /// Gets the old value of the item.
         /// </summary>
         public V OldValue { get; }
 
         /// <summary>
-        /// Gets the new value of the item that was added to the cache.
+        /// Gets the new value of the item.
         /// </summary>
         public V NewValue { get; }
     }
