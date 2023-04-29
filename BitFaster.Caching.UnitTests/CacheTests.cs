4e2dbaab3a1b1844839c4889828052fc8f324618
Alex Peck <alexpeck@microsoft.com>
2023-04-28 18:51:30 -0700

    cache default

---
+++ BitFaster.Caching.UnitTests/CacheTests.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace BitFaster.Caching.UnitTests
+{
+    public class CacheTests
+    {
+// backcompat: remove conditional compile
+#if NETCOREAPP3_0_OR_GREATER
+        [Fact]
+        public void WhenInterfaceDefaultGetOrAddFallback()
+        {
+            var cache = new Mock<ICache<int, int>>();
+            cache.CallBase = true;
+
+            Func<int, Func<int, int>, int> evaluate = (k, f) => f(k);
+            cache.Setup(c => c.GetOrAdd(It.IsAny<int>(), It.IsAny<Func<int, int>>())).Returns(evaluate);
+
+            cache.Object.GetOrAdd(
+                1, 
+                (k, a) => k + a, 
+                2).Should().Be(3);
+        }
+#endif
+    }
+}
