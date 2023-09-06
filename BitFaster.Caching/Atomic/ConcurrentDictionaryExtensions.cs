e086194db149748dc4cd22a51428dec21bde0ea4
Alex Peck <alexpeck@microsoft.com>
2023-09-06 13:13:22 -0700

    ext

---
+++ BitFaster.Caching/Atomic/ConcurrentDictionaryExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace BitFaster.Caching.Atomic
+{
+    public static class ConcurrentDictionaryExtensions
+    {
+        public static V GetOrAdd<K, V>(this ConcurrentDictionary<K, AtomicFactory<K, V>> cache, K key, Func<K, V> valueFactory)
+        {
+            var atomicFactory = cache.GetOrAdd(key, _ => new AtomicFactory<K, V>());
+            return atomicFactory.GetValue(key, valueFactory);
+        }
+    }
+}
