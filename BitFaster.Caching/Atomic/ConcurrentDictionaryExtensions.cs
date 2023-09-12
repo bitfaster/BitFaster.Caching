49bf25e30daab1ff1824bc314a4d6d75e95cbe09
Alex Peck <alexpeck@microsoft.com>
2023-09-11 22:37:30 -0700

    arg

--- BitFaster.Caching/Atomic/ConcurrentDictionaryExtensions.cs
+++ BitFaster.Caching/Atomic/ConcurrentDictionaryExtensions.cs
@@ -3,12 +3,36 @@ using System.Collections.Concurrent;
 
 namespace BitFaster.Caching.Atomic
 {
+    /// <summary>
+    /// Convenience methods for using AtomicFactory with ConcurrentDictionary. 
+    /// </summary>
     public static class ConcurrentDictionaryExtensions
     {
         public static V GetOrAdd<K, V>(this ConcurrentDictionary<K, AtomicFactory<K, V>> cache, K key, Func<K, V> valueFactory)
         {
             var atomicFactory = cache.GetOrAdd(key, _ => new AtomicFactory<K, V>());
             return atomicFactory.GetValue(key, valueFactory);
+        }
+
+        public static V GetOrAdd<K, V, TArg>(this ConcurrentDictionary<K, AtomicFactory<K, V>> cache, K key, Func<K, TArg, V> valueFactory, TArg factoryArgument)
+        {
+            var atomicFactory = cache.GetOrAdd(key, _ => new AtomicFactory<K, V>());
+            return atomicFactory.GetValue(key, valueFactory, factoryArgument);
+        }
+
+        public static bool TryGetValue<K, V>(this ConcurrentDictionary<K, AtomicFactory<K, V>> cache, K key, out V value)
+        {
+            AtomicFactory<K, V> output;
+            var ret = cache.TryGetValue(key, out output);
+
+            if (ret && output.IsValueCreated)
+            {
+                value = output.ValueIfCreated;
+                return true;
+            }
+
+            value = default;
+            return false;
         }
     }
 }
