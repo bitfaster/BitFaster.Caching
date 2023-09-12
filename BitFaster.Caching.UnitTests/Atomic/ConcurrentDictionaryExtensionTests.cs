a8c58bc22095eccf56022f64f514550c2c263d45
Alex Peck <alexpeck@microsoft.com>
2023-09-12 14:31:54 -0700

    docs+test

---
+++ BitFaster.Caching.UnitTests/Atomic/ConcurrentDictionaryExtensionTests.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Collections.Concurrent;
+using BitFaster.Caching.Atomic;
+using FluentAssertions;
+using Xunit;
+
+namespace BitFaster.Caching.UnitTests.Atomic
+{
+    public class ConcurrentDictionaryExtensionTests
+    {
+        private ConcurrentDictionary<int, AtomicFactory<int, int>> dictionary = new ConcurrentDictionary<int, AtomicFactory<int, int>>();
+
+        [Fact]
+        public void GetOrAdd()
+        {
+            dictionary.GetOrAdd(1, k => k);
+
+            dictionary.TryGetValue(1, out int value).Should().BeTrue();
+            value.Should().Be(1);
+        }
+    }
+}
