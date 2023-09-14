faa984f296aa448f17101f6a7c08769874e24aed
Alex Peck <alexpeck@microsoft.com>
2023-09-13 22:44:20 -0700

    tests

--- BitFaster.Caching.UnitTests/Atomic/ConcurrentDictionaryExtensionTests.cs
+++ BitFaster.Caching.UnitTests/Atomic/ConcurrentDictionaryExtensionTests.cs
@@ -1,5 +1,6 @@
 ﻿
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using BitFaster.Caching.Atomic;
 using FluentAssertions;
 using Xunit;
@@ -11,12 +12,39 @@ namespace BitFaster.Caching.UnitTests.Atomic
         private ConcurrentDictionary<int, AtomicFactory<int, int>> dictionary = new ConcurrentDictionary<int, AtomicFactory<int, int>>();
 
         [Fact]
-        public void GetOrAdd()
+        public void WhenItemIsAddedItCanBeRetrieved()
         {
             dictionary.GetOrAdd(1, k => k);
 
             dictionary.TryGetValue(1, out int value).Should().BeTrue();
             value.Should().Be(1);
+        }
+
+        [Fact]
+        public void WhenItemIsAddedWithArgItCanBeRetrieved()
+        {
+            dictionary.GetOrAdd(1, (k,a) => k + a, 2);
+
+            dictionary.TryGetValue(1, out int value).Should().BeTrue();
+            value.Should().Be(3);
+        }
+
+        [Fact]
+        public void WhenItemIsAddedItCanBeRemovedByKey()
+        {
+            dictionary.GetOrAdd(1, k => k);
+
+            dictionary.TryRemove(1, out int value).Should().BeTrue();
+            value.Should().Be(1);
+        }
+
+        [Fact]
+        public void WhenItemIsAddedItCanBeRemovedByKvp()
+        {
+            dictionary.GetOrAdd(1, k => k);
+
+            dictionary.TryRemove(new KeyValuePair<int, int>(1, 1)).Should().BeTrue();
+            dictionary.TryGetValue(1, out _).Should().BeFalse();
         }
     }
 }
