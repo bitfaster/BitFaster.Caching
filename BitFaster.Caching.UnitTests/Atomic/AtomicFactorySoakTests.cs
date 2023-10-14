dd39d618e7d1179c134a470556358b805f864b8b
Alex Peck <alexpeck@microsoft.com>
2023-10-14 16:48:15 -0700

    atomicFactory

---
+++ BitFaster.Caching.UnitTests/Atomic/AtomicFactorySoakTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using BitFaster.Caching.Lru;
+using FluentAssertions;
+using Xunit;
+
+namespace BitFaster.Caching.UnitTests.Atomic
+{
+    [Collection("Soak")]
+    public class AtomicFactorySoakTests
+    {
+        [Fact]
+        public async Task WhenGetOrAddIsConcurrentValuesCreatedAtomically()
+        {
+            var cache = new ConcurrentLruBuilder<int, int>().WithAtomicGetOrAdd().WithCapacity(1024).Build();
+
+            var counters = new int[4];
+
+            await Threaded.Run(4, (r) =>
+            {
+                for (int i = 0; i < 1024; i++)
+                {
+                    cache.GetOrAdd(i, k => { counters[r]++; return k; });
+                }
+            });
+
+            counters.Sum(x => x).Should().Be(1024);
+        }
+    }
+}
