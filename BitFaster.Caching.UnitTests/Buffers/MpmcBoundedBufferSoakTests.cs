a3246c2895e15dd67a624ca1bc727a3bf3910907
Alex Peck <alexpeck@microsoft.com>
2023-10-14 15:38:09 -0700

    soak mpmcbuffer

---
+++ BitFaster.Caching.UnitTests/Buffers/MpmcBoundedBufferSoakTests.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using BitFaster.Caching.Buffers;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace BitFaster.Caching.UnitTests.Buffers
+{
+    [Collection("Soak")]
+    public class MpmcBoundedBufferSoakTests
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+        private static readonly TimeSpan Timeout = TimeSpan.FromSeconds(30);
+
+        private readonly MpmcBoundedBuffer<string> buffer = new MpmcBoundedBuffer<string>(1024);
+
+        public MpmcBoundedBufferSoakTests(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public async Task WhenAddIsContendedBufferCanBeFilled()
+        {
+            await Threaded.Run(4, () =>
+            {
+                while (buffer.TryAdd("hello") != BufferStatus.Full)
+                {
+                }
+
+                buffer.Count.Should().Be(1024);
+            });
+        }
+
+        [Fact]
+        public async Task WhileBufferIsFilledItemsCanBeTaken()
+        {
+            this.testOutputHelper.WriteLine($"ProcessorCount={Environment.ProcessorCount}.");
+
+            var fill = CreateParallelFill(buffer, threads: 4, itemsPerThread: 256);
+
+            var take = Threaded.Run(4, () =>
+            {
+                var spin = new SpinWait();
+                int count = 0;
+                while (count < 256)
+                {
+                    while (true)
+                    {
+                        if (buffer.TryTake(out _) == BufferStatus.Success)
+                        {
+                            break;
+                        }
+                        spin.SpinOnce();
+                    }
+                    count++;
+                }
+            });
+
+            await fill.TimeoutAfter(Timeout, "fill timed out");
+            await take.TimeoutAfter(Timeout, "take timed out");
+        }
+
+        [Fact]
+        public async Task WhileBufferIsFilledCountCanBeTaken()
+        {
+            this.testOutputHelper.WriteLine($"ProcessorCount={Environment.ProcessorCount}.");
+
+            var fill = CreateParallelFill(buffer, threads: 4, itemsPerThread: 256);
+
+            var count = Task.Run(() =>
+            {
+                int count = 0;
+
+                while (!fill.IsCompleted)
+                {
+                    int newcount = buffer.Count;
+                    newcount.Should().BeGreaterThanOrEqualTo(count);
+                    count = newcount;
+                }
+            });
+
+            await fill.TimeoutAfter(Timeout, "fill timed out");
+            await count.TimeoutAfter(Timeout, "count timed out");
+        }
+
+        private Task CreateParallelFill(MpmcBoundedBuffer<string> buffer, int threads, int itemsPerThread)
+        {
+            return Threaded.Run(threads, () =>
+            {
+                var spin = new SpinWait();
+                int count = 0;
+                while (count < itemsPerThread)
+                {
+                    while (true)
+                    {
+                        if (buffer.TryAdd("hello") == BufferStatus.Success)
+                        {
+                            break;
+                        }
+                        spin.SpinOnce();
+                    }
+                    count++;
+                }
+            });
+        }
+    }
+}
