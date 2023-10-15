23c81b7a6b3fe57b99b3915e966a7c112c3bf737
Alex Peck <alexpeck@microsoft.com>
2023-10-14 23:40:51 -0700

    comments

--- BitFaster.Caching/HashTablePrimes.cs
+++ BitFaster.Caching/HashTablePrimes.cs
@@ -1,26 +1,18 @@
 ﻿using System;
-using System.Buffers.Text;
-using System.Collections.Concurrent;
-using System.Collections;
-using System.Collections.Generic;
-using System.Drawing;
-using System.IO;
-using System.Reflection.Metadata;
-using System.Runtime.Intrinsics.X86;
-using System.Text;
-using static BitFaster.Caching.Buffers.MpmcBoundedBuffer<T>;
-using static System.Net.Mime.MediaTypeNames;
-using System.Text.RegularExpressions;
+
 namespace BitFaster.Caching
 {
-    // Using the capacity passed into the cache ctor to initialize the ConcurrentDictionary  has 2 problems:
+    // Using the capacity passed into the cache ctor to initialize the ConcurrentDictionary has 2 problems:
     //
-    // 1. By allocating up front, we eliminate resizing.However, if the capacity is very large and the cache is not used,
+    // 1. By allocating up front, we eliminate resizing. However, if the capacity is very large and the cache is not used,
     // we will waste a lot of memory.
     // 2. On earlier versions of .NET, ConcurrentDictionary uses the capacity arg to directly initialize the hash table
-    // size (as noted here). On resize, the hashtable is grown to 2x + 1 while avoiding factors of 3, 5, or 7 (but not larger). On newer versions of.NET, both initial size and resize is based the next prime number larger than capacity. Collisions are reduced when hash table size is prime, as noted here. Hence the change to use primes in all cases in newer versions of the framework.
+    // size. On resize, the hashtable is grown to 2x + 1 while avoiding factors of 3, 5, or 7 (but not larger). On
+    // newer versions of.NET, both initial size and resize is based the next prime number larger than capacity. Collisions
+    // are reduced when hash table size is prime. Hence the change to use primes in all cases in newer versions of the
+    // framework.
     //
-    // To mitigate these issues, adopt a simple scheme: find the next prime larger than the capacity arg, up to 137. If the
+    // To mitigate this, we adopt a simple scheme: find the next prime larger than the capacity arg, up to 137. If the
     // capacity is greater than 137, just set the initial size to 137, thereby bounding initial memory consumption for
     // large caches.
     // 
