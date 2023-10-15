eb293ef5b14ef88dce65f6f2eb4cc2220e6e200e
Alex Peck <alexpeck@microsoft.com>
2023-10-14 22:25:05 -0700

    tests

---
+++ BitFaster.Caching.UnitTests/HashTablePrimesTests.cs
@@ -0,0 +1,20 @@
+﻿using BitFaster.Caching;
+using FluentAssertions;
+using Xunit;
+
+namespace BitFaster.Caching.UnitTests
+{
+    public class HashTablePrimesTests
+    {
+        [Theory]
+        [InlineData(3, 7)]
+        [InlineData(8, 11)]
+        [InlineData(12, 17)]
+        [InlineData(132, 131)]
+        [InlineData(500, 131)]
+        public void NextPrimeGreaterThan(int input, int nextPrime)
+        {
+            HashTablePrimes.NextPrimeGreaterThan(input).Should().Be(nextPrime);
+        }
+    }
+}
