2f100c96427e2e2908f620acb5f7209f4efc6a9c
Alex Peck <alexpeck@microsoft.com>
2023-10-14 17:21:51 -0700

    test nodelist

---
+++ BitFaster.Caching.UnitTests/Lfu/LfuNodeListTests.cs
@@ -0,0 +1,41 @@
+﻿using BitFaster.Caching.Lfu;
+using FluentAssertions;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BitFaster.Caching.UnitTests.Lfu
+{
+    public class LfuNodeListTests
+    {
+        [Fact]
+        public void WhenPreviousExistsPreviousReturnsPrevious()
+        {
+            var list = new LfuNodeList<int, int>();
+            var node1 = new LfuNode<int, int>(1, 1);
+            var node2 = new LfuNode<int, int>(2, 2);
+
+            list.AddLast(node1);
+            list.AddLast(node2);
+
+            node2.Previous.Should().BeSameAs(node1);
+        }
+
+        [Fact]
+        public void WhenHeadPreviousReturnsNull()
+        {
+            var list = new LfuNodeList<int, int>();
+            var node1 = new LfuNode<int, int>(1, 1);
+            var node2 = new LfuNode<int, int>(2, 2);
+
+            list.AddLast(node1);
+            list.AddLast(node2);
+
+            node1.Previous.Should().BeNull();
+        }
+    }
+}
