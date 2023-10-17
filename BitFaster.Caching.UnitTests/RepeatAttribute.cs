6c738184952f335a1c7c38c8fe4bc23481a74e01
Alex Peck <amwpeck@gmail.com>
2023-10-17 13:20:34 -0700

    repeat tests to stabilize coverage (#429)
    * repeat
    
    * repeat lfu
    
    * undo lfu
    
    ---------

---
+++ BitFaster.Caching.UnitTests/RepeatAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+
+namespace BitFaster.Caching.UnitTests
+{
+    public sealed class RepeatAttribute : Xunit.Sdk.DataAttribute
+    {
+        private readonly int count;
+
+        public RepeatAttribute(int count)
+        {
+            if (count < 1)
+            {
+                throw new System.ArgumentOutOfRangeException(
+                    paramName: nameof(count),
+                    message: "Repeat count must be greater than 0."
+                    );
+            }
+            this.count = count;
+        }
+
+        public override System.Collections.Generic.IEnumerable<object[]> GetData(System.Reflection.MethodInfo testMethod)
+        {
+            foreach (var iterationNumber in Enumerable.Range(start: 1, count: this.count))
+            {
+                yield return new object[] { iterationNumber };
+            }
+        }
+    }
+}
