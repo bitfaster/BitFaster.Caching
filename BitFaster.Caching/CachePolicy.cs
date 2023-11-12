fd5fdb1012a395b6f6068d92f3e6473a98b22a5e
Alex Peck <alexpeck@microsoft.com>
2023-11-11 18:49:38 -0800

    docs

--- BitFaster.Caching/CachePolicy.cs
+++ BitFaster.Caching/CachePolicy.cs
@@ -19,16 +19,16 @@ namespace BitFaster.Caching
         }
 
         /// <summary>
-        /// Initializes a new instance of the CachePolicy class with the specified eviction and expire after write policies.
+        /// Initializes a new instance of the CachePolicy class with the specified policies.
         /// </summary>
         /// <param name="eviction">The eviction policy.</param>
         /// <param name="expireAfterWrite">The expire after write policy.</param>
-        /// <param name="expireAfterRead">The expire after read policy.</param>
-        public CachePolicy(Optional<IBoundedPolicy> eviction, Optional<ITimePolicy> expireAfterWrite, Optional<ITimePolicy> expireAfterRead)
+        /// <param name="expireAfterAccess">The expire after access policy.</param>
+        public CachePolicy(Optional<IBoundedPolicy> eviction, Optional<ITimePolicy> expireAfterWrite, Optional<ITimePolicy> expireAfterAccess)
         {
             this.Eviction = eviction;
             this.ExpireAfterWrite = expireAfterWrite;
-            this.ExpireAfterAccess = expireAfterRead;
+            this.ExpireAfterAccess = expireAfterAccess;
         }
 
         /// <summary>
@@ -44,8 +44,8 @@ namespace BitFaster.Caching
         public Optional<ITimePolicy> ExpireAfterWrite { get; }
 
         /// <summary>
-        /// Gets the expire after read policy, if any. This policy evicts items after a 
-        /// fixed duration since an entry's creation or most recent read.
+        /// Gets the expire after access policy, if any. This policy evicts items after a 
+        /// fixed duration since an entry's creation or most recent read/write access.
         /// </summary>
         public Optional<ITimePolicy> ExpireAfterAccess { get; }
     }
