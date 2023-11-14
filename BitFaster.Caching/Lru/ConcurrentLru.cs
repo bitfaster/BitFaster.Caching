530368e271e1697966a80a63f454a127f22e6744
Alex Peck <amwpeck@gmail.com>
2023-11-14 11:21:47 -0800

    Rename factory (#482)

--- BitFaster.Caching/Lru/ConcurrentLru.cs
+++ BitFaster.Caching/Lru/ConcurrentLru.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq.Expressions;
+﻿
 using BitFaster.Caching.Lru.Builder;
 
 namespace BitFaster.Caching.Lru
@@ -6,14 +6,14 @@ namespace BitFaster.Caching.Lru
     /// <summary>
     /// Factory class for creating ConcurrentLru variants.
     /// </summary>
-    internal static class LruFactory<K, V>
+    internal static class ConcurrentLru
     {
         /// <summary>
         /// Creates a ConcurrentLru instance based on the provided LruInfo.
         /// </summary>
         /// <param name="info">The LruInfo</param>
         /// <returns>A ConcurrentLru</returns>
-        internal static ICache<K, V> CreateConcurrent(LruInfo<K> info)
+        internal static ICache<K, V> Create<K, V>(LruInfo<K> info)
         {
             if (info.TimeToExpireAfterWrite.HasValue && info.TimeToExpireAfterAccess.HasValue)
                 Throw.InvalidOp("Specifying both ExpireAfterWrite and ExpireAfterAccess is not supported.");
@@ -23,13 +23,13 @@ namespace BitFaster.Caching.Lru
                 (true, false, false) => new ConcurrentLru<K, V>(info.ConcurrencyLevel, info.Capacity, info.KeyComparer),
                 (true, true, false) => new ConcurrentTLru<K, V>(info.ConcurrencyLevel, info.Capacity, info.KeyComparer, info.TimeToExpireAfterWrite.Value),
                 (false, true, false) => new FastConcurrentTLru<K, V>(info.ConcurrencyLevel, info.Capacity, info.KeyComparer, info.TimeToExpireAfterWrite.Value),
-                (true, false, true) => CreateExpireAfterAccess<TelemetryPolicy<K, V>>(info),
-                (false, false, true) => CreateExpireAfterAccess<NoTelemetryPolicy<K, V>>(info),
+                (true, false, true) => CreateExpireAfterAccess<K, V, TelemetryPolicy<K, V>>(info),
+                (false, false, true) => CreateExpireAfterAccess<K, V, NoTelemetryPolicy<K, V>>(info),
                 _ => new FastConcurrentLru<K, V>(info.ConcurrencyLevel, info.Capacity, info.KeyComparer),
             };
         }
 
-        private static ICache<K, V> CreateExpireAfterAccess<TP>(LruInfo<K> info) where TP : struct, ITelemetryPolicy<K, V>
+        private static ICache<K, V> CreateExpireAfterAccess<K, V, TP>(LruInfo<K> info) where TP : struct, ITelemetryPolicy<K, V>
         {
             return new ConcurrentLruCore<K, V, LongTickCountLruItem<K, V>, AfterAccessLongTicksPolicy<K, V>, TP>(
                 info.ConcurrencyLevel, info.Capacity, info.KeyComparer, new AfterAccessLongTicksPolicy<K, V>(info.TimeToExpireAfterAccess.Value), default);
