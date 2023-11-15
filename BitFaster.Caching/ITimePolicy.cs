6c7b37025a8f8597833b464bea7fc3b367937ba3
Alex Peck <alexpeck@microsoft.com>
2023-11-15 12:06:33 -0800

    file

--- BitFaster.Caching/ITimePolicy.cs
+++ BitFaster.Caching/ITimePolicy.cs
@@ -17,23 +17,4 @@ namespace BitFaster.Caching
         /// </summary>
         void TrimExpired();
     }
-
-    /// <summary>
-    /// Represents a per item time based cache policy.
-    /// </summary>
-    public interface IDiscreteTimePolicy
-    {
-        /// <summary>
-        /// Gets the time to live for an item in the cache.
-        /// </summary>
-        /// <param name="key">The key of the item.</param>
-        /// <param name="timeToExpire">If the key exists, the time to live for the item with the specified key.</param>
-        /// <returns>True if the key exists, otherwise false.</returns>
-        bool TryGetTimeToExpire<K>(K key, out TimeSpan timeToExpire);
-
-        /// <summary>
-        /// Remove all expired items from the cache.
-        /// </summary>
-        void TrimExpired();
-    }
 }
