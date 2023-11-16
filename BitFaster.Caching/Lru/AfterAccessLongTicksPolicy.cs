9fbf53e735bfe5844c618b04b07a6f32ed92485b
Alex Peck <amwpeck@gmail.com>
2023-11-16 10:07:05 -0800

    Convert ExpireAfterAccess policy to Duration (#486)

--- BitFaster.Caching/Lru/AfterAccessLongTicksPolicy.cs
+++ BitFaster.Caching/Lru/AfterAccessLongTicksPolicy.cs
@@ -1,25 +1,18 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Runtime.CompilerServices;
 
 namespace BitFaster.Caching.Lru
 {
-#if NETCOREAPP3_0_OR_GREATER
     /// <summary>
     /// Implement an expire after access policy.
     /// </summary>
-    /// <remarks>
-    /// This class measures time using Environment.TickCount64, which is significantly faster
-    /// than both Stopwatch.GetTimestamp and DateTime.UtcNow. However, resolution is lower (typically 
-    /// between 10-16ms), vs 1us for Stopwatch.GetTimestamp.
-    /// </remarks>
     internal readonly struct AfterAccessLongTicksPolicy<K, V> : IItemPolicy<K, V, LongTickCountLruItem<K, V>>
     {
-        private readonly long timeToLive;
+        private readonly Duration timeToLive;
         private readonly Time time;
 
         ///<inheritdoc/>
-        public TimeSpan TimeToLive => TimeSpan.FromMilliseconds(timeToLive);
+        public TimeSpan TimeToLive => timeToLive.ToTimeSpan();
 
         /// <summary>
         /// Initializes a new instance of the AfterReadTickCount64Policy class with the specified time to live.
@@ -30,7 +23,7 @@ namespace BitFaster.Caching.Lru
             if (timeToLive <= TimeSpan.Zero || timeToLive > Time.MaxRepresentable)
                 Throw.ArgOutOfRange(nameof(timeToLive), $"Value must greater than zero and less than {Time.MaxRepresentable}");
 
-            this.timeToLive = (long)timeToLive.TotalMilliseconds;
+            this.timeToLive = Duration.FromTimeSpan(timeToLive);
             this.time = new Time();
         }
 
@@ -38,7 +31,7 @@ namespace BitFaster.Caching.Lru
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public LongTickCountLruItem<K, V> CreateItem(K key, V value)
         {
-            return new LongTickCountLruItem<K, V>(key, value, Environment.TickCount64);
+            return new LongTickCountLruItem<K, V>(key, value, Duration.SinceEpoch().raw);
         }
 
         ///<inheritdoc/>
@@ -53,15 +46,15 @@ namespace BitFaster.Caching.Lru
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void Update(LongTickCountLruItem<K, V> item)
         {
-            item.TickCount = Environment.TickCount64;
+            item.TickCount = Duration.SinceEpoch().raw;
         }
 
         ///<inheritdoc/>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public bool ShouldDiscard(LongTickCountLruItem<K, V> item)
         {
-            this.time.Last = Environment.TickCount64;
-            if (this.time.Last - item.TickCount > this.timeToLive)
+            this.time.Last = Duration.SinceEpoch().raw;
+            if (this.time.Last - item.TickCount > this.timeToLive.raw)
             {
                 return true;
             }
@@ -127,5 +120,4 @@ namespace BitFaster.Caching.Lru
             return ItemDestination.Remove;
         }
     }
-#endif
 }
