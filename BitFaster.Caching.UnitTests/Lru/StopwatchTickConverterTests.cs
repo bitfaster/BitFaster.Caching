4a2096c226799f1ff26511572224a98d64f66ecb
Alex Peck <alexpeck@microsoft.com>
2023-11-16 17:53:19 -0800

    converter

---
+++ BitFaster.Caching.UnitTests/Lru/StopwatchTickConverterTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BitFaster.Caching.Lru;
+using FluentAssertions;
+using Xunit;
+
+namespace BitFaster.Caching.UnitTests.Lru
+{
+    public class StopwatchTickConverterTests
+    {
+        [Fact]
+        public void WhenConvertingToTicksIsReversable()
+        { 
+            var timespan = TimeSpan.FromSeconds(1);
+
+            StopwatchTickConverter.FromTicks(StopwatchTickConverter.ToTicks(timespan)).Should().BeCloseTo(timespan, TimeSpan.FromMilliseconds(20));
+        }
+    }
+}
