ccafac024cdf1dcf058b51a7a5b247ddd9b0006c
Alex Peck <amwpeck@gmail.com>
2023-11-18 15:17:13 -0800

    Remove CachePolicy TryTrimExpired (#499)

--- BitFaster.Caching.UnitTests/CachePolicyTests.cs
+++ BitFaster.Caching.UnitTests/CachePolicyTests.cs
@@ -19,40 +19,5 @@ namespace BitFaster.Caching.UnitTests
             cp.ExpireAfterAccess.HasValue.Should().BeFalse();
             cp.ExpireAfter.HasValue.Should().BeFalse();
         }
-
-        [Fact]
-        public void TryTrimWhenTrimNotSupportedReturnsFalse()
-        {
-            var cp = new CachePolicy(Optional<IBoundedPolicy>.None(), Optional<ITimePolicy>.None());
-
-            cp.TryTrimExpired().Should().BeFalse();
-        }
-
-        [Fact]
-        public void TryTrimWhenExpireAfterWriteReturnsTrue()
-        {
-            var expire = new Mock<ITimePolicy>();
-            var cp = new CachePolicy(Optional<IBoundedPolicy>.None(), new Optional<ITimePolicy>(expire.Object));
-
-            cp.TryTrimExpired().Should().BeTrue();
-        }
-
-        [Fact]
-        public void TryTrimWhenExpireAfterAccessReturnsTrue()
-        {
-            var expire = new Mock<ITimePolicy>();
-            var cp = new CachePolicy(Optional<IBoundedPolicy>.None(), Optional<ITimePolicy>.None(), new Optional<ITimePolicy>(expire.Object), Optional<IDiscreteTimePolicy>.None());
-
-            cp.TryTrimExpired().Should().BeTrue();
-        }
-
-        [Fact]
-        public void TryTrimWhenExpireAfterReturnsTrue()
-        {
-            var expire = new Mock<IDiscreteTimePolicy>();
-            var cp = new CachePolicy(Optional<IBoundedPolicy>.None(), Optional<ITimePolicy>.None(), Optional<ITimePolicy>.None(), new Optional<IDiscreteTimePolicy>(expire.Object));
-
-            cp.TryTrimExpired().Should().BeTrue();
-        }
     }
 }
