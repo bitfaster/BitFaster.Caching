28e7203b0e40e47a19251e3d9115b2cef3efe38d
Alex Peck <amwpeck@gmail.com>
2023-12-03 23:31:52 -0800

    Print branch info to command line (#529)

---
+++ BitFaster.Caching.HitRateAnalysis/Splash.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BitFaster.Caching.HitRateAnalysis
+{
+    internal static class Splash
+    {
+        public static void Display()
+        {
+            string branch = $"({ThisAssembly.Git.Branch}" + (ThisAssembly.Git.IsDirty ? " dirty)" : ")");
+
+            Console.WriteLine($"Hit Rate Analysis {ThisAssembly.Git.BaseTag} {ThisAssembly.Git.Commit} {branch}");
+            Console.WriteLine();
+        }
+    }
+}
