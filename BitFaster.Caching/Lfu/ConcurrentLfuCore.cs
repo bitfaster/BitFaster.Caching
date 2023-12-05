53e26257d899a1cc7c2081497ca28af7d85b1c29
Alex Peck <alexpeck@microsoft.com>
2023-12-04 17:42:32 -0800

    probation

--- BitFaster.Caching/Lfu/ConcurrentLfuCore.cs
+++ BitFaster.Caching/Lfu/ConcurrentLfuCore.cs
@@ -609,12 +609,6 @@ namespace BitFaster.Caching.Lfu
 
         private void PromoteProbation(LfuNode<K, V> node)
         {
-            //if (node.list == null)
-            //{
-            //    // Ignore stale accesses for an entry that is no longer present
-            //    return;
-            //}
-
             this.probationLru.Remove(node);
             this.protectedLru.AddLast(node);
             node.Position = Position.Protected;
