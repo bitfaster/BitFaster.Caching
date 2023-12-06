13f2de9450784703283b9106205bedd51d3cd870
Alex Peck <alexpeck@microsoft.com>
2023-12-06 11:06:06 -0800

    atomic not null

--- BitFaster.Caching/Atomic/AtomicFactory.cs
+++ BitFaster.Caching/Atomic/AtomicFactory.cs
@@ -14,6 +14,7 @@ namespace BitFaster.Caching.Atomic
     /// <typeparam name="V">The type of the value.</typeparam>
     [DebuggerDisplay("IsValueCreated={IsValueCreated}, Value={ValueIfCreated}")]
     public sealed class AtomicFactory<K, V> : IEquatable<AtomicFactory<K, V>>
+        where K : notnull
     {
         private Initializer initializer;
 
