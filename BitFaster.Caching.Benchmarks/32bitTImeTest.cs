c5c56ebd22eeec47dafe4793a38e8c05e5a82fe1
Alex Peck <alexpeck@microsoft.com>
2024-05-17 20:21:00 -0700

    torn

--- BitFaster.Caching.Benchmarks/32bitTImeTest.cs
+++ BitFaster.Caching.Benchmarks/32bitTImeTest.cs
@@ -1,18 +1,40 @@
-﻿
-using System;
+﻿using System;
 using System.Runtime.CompilerServices;
 using System.Threading;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
 
-namespace BitFaster.Caching.Lru
+namespace BitFaster.Caching.Benchmarks
 {
-    /// <summary>
-    /// During reads, the policy evaluates ShouldDiscard and Touch. To avoid Getting the current time twice
-    /// introduce a simple time class that holds the last time. This is class with a mutable field, because the 
-    /// policy structs are readonly.
-    /// </summary>
-    /// <remarks>
-    /// This class mitigates torn writes when running on 32-bit systems using Interlocked read and write.
-    /// </remarks>
+#if Windows
+    [DisassemblyDiagnoser(printSource: true, maxDepth: 5)]
+    [SimpleJob(RuntimeMoniker.Net48)]
+#endif
+    [SimpleJob(RuntimeMoniker.Net60)]
+    [HideColumns("Job", "Median", "RatioSD", "Alloc Ratio")]
+    public class _32bitTImeTest
+    {
+        private static TimeOrig timeOrig = new TimeOrig();
+        private static Time time = new Time();
+
+        [Benchmark(Baseline =true)]
+        public long TimeOriginal()
+        { 
+            return timeOrig.Last; 
+        }
+
+        [Benchmark()]
+        public long Time2()
+        { 
+            return time.Last; 
+        }
+    }
+
+    internal class TimeOrig
+    { 
+        internal long Last { get; set;}
+    }
+
     internal class Time
     {
         private static readonly bool Is64Bit = Environment.Is64BitProcess;
