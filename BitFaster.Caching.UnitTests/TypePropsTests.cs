bab2c0e348488541af7494a9ae3fc21f8b2fc12c
Alex Peck <alexpeck@microsoft.com>
2024-05-18 20:24:35 -0700

    test types

---
+++ BitFaster.Caching.UnitTests/TypePropsTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+using FluentAssertions;
+using Xunit;
+
+namespace BitFaster.Caching.UnitTests
+{
+    public class TypePropsTests
+    {
+        [Theory]
+        [InlineData(typeof(object), true)]
+        [InlineData(typeof(IntPtr), true)]
+        [InlineData(typeof(UIntPtr), true)]
+        [InlineData(typeof(int), true)]
+        [InlineData(typeof(Guid), false)]
+        public void Test(Type argType, bool expected)
+        { 
+            MethodInfo method = typeof(TypePropsTests).GetMethod(nameof(TypePropsTests.IsWriteAtomic), BindingFlags.NonPublic | BindingFlags.Static);
+            MethodInfo generic = method.MakeGenericMethod(argType);
+
+            generic.Invoke(null, null).Should().BeOfType<bool>().Which.Should().Be(expected);
+        }
+
+        private static bool IsWriteAtomic<T>()
+        { 
+            return TypeProps<T>.IsWriteAtomic;
+        }
+    }
+}
