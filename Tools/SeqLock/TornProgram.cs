84155675ccccfdcf20e1f890587b43ab71068267
Alex Peck <alexpeck@microsoft.com>
2024-05-21 11:49:45 -0700

    matching repro

--- Tools/SeqLock/TornProgram.cs
+++ Tools/SeqLock/TornProgram.cs
@@ -16,8 +16,8 @@
         {
             while (true)
             {
-                d = VALUE1;
-                d = VALUE2;
+                d1.Value = VALUE1;
+                d1.Value = VALUE2;
             }
         }
 
@@ -25,14 +25,14 @@
         {
             for (int count = 0;; ++count)
             {
-                var t = d;
+                var t = d1.Value;
 
                 if (t != VALUE1 && t != VALUE2)
                     Console.WriteLine("Value is torn after {0} iterations: {1}", count, t);
             }
         }
 
-        Decimal d;
+        LruItem<int, Decimal> d1 = new LruItem<int, decimal>(1, VALUE1);
 
         const Decimal VALUE1 = 1m;
         const Decimal VALUE2 = 10000000000m;
