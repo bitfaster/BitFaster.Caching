019070af9d424c52022455c81c4ae7f6de4e1be3
Alex Peck <amwpeck@gmail.com>
2025-06-28 19:17:29 -0700

    add net9 target and tests (#692)
    * net9
    
    * different build leg
    
    * fix proj
    
    * fix test
    
    * default concurrency
    
    * correct version
    
    * more tests
    
    * cleanup
    
    * publish coverage for net6

--- BitFaster.Caching/Lru/Defaults.cs
+++ BitFaster.Caching/Lru/Defaults.cs
@@ -1,11 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace BitFaster.Caching.Lru
 {
     internal static class Defaults
     {
+#if NET8_0_OR_GREATER
+        // Note that on .net8+, -1 indicates the default concurrency level
+        public static int ConcurrencyLevel => -1;
+#else
         public static int ConcurrencyLevel => Environment.ProcessorCount;
+#endif
     }
 }
