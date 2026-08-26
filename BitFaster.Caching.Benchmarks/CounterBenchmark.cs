6d3c1209a637906b2fc77cf68e107e8f1882f0e2
Alex Peck <alexpeck@microsoft.com>
2026-08-25 21:24:22 -0700

    bench

---
+++ BitFaster.Caching.Benchmarks/CounterBenchmark.cs
@@ -0,0 +1,111 @@
+﻿
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Diagnostics.Tracing;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+
+namespace BitFaster.Caching.Benchmarks
+{
+#if Windows
+    [DisassemblyDiagnoser(printSource: true, maxDepth: 5)]
+#endif
+    [HideColumns("Job", "Median", "RatioSD", "Alloc Ratio")]
+    public class CounterBenchmark
+    {
+        const int Iters = 1_000_000;
+
+        private Counters.Counter counter = new Counters.Counter();
+
+        private Meter meter;
+        private Counter<long> metricsCounter;
+        private UpDownCounter<long> upDownCounter;
+        private MetricsEventListener listener;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            meter = new Meter("Example");
+            upDownCounter = meter.CreateUpDownCounter<long>("upDownCounter");
+            metricsCounter = meter.CreateCounter<long>("counter");
+            listener = new MetricsEventListener();
+        }
+
+        [GlobalCleanup]
+        public void Cleanup()
+        {
+            meter.Dispose();
+        }
+
+        [Benchmark]
+        public void CounterSerial()
+        {
+            for (int i = 0; i < Iters; i++)
+            {
+                counter.Add(1);
+                counter.Add(1);
+            }
+        }
+
+        [Benchmark]
+        public void CounterParallel()
+        {
+            Parallel.For(0, Iters, i =>
+            {
+                counter.Add(1);
+                counter.Add(1);
+            });
+        }
+
+        [Benchmark]
+        public void MetricsCounterSerial()
+        {
+            for (int i = 0; i < Iters; i++)
+            {
+                metricsCounter.Add(1);
+                metricsCounter.Add(1);
+            }
+        }
+
+        [Benchmark]
+        public void MetricsCounterParallel()
+        {
+            Parallel.For(0, Iters, i =>
+            {
+                metricsCounter.Add(1);
+                metricsCounter.Add(1);
+            });
+        }
+
+        [Benchmark]
+        public void UpDownCounterSerial()
+        {
+            for (int i = 0; i < Iters; i++)
+            {
+                upDownCounter.Add(1);
+                upDownCounter.Add(-1);
+            }
+        }
+
+        [Benchmark]
+        public void UpDownCounterParallel()
+        {
+            Parallel.For(0, Iters, i =>
+            {
+                upDownCounter.Add(1);
+                upDownCounter.Add(-1);
+            });
+        }
+
+        private sealed class MetricsEventListener : EventListener
+        {
+            protected override void OnEventSourceCreated(EventSource eventSource)
+            {
+                if (eventSource.Name == "System.Diagnostics.Metrics")
+                {
+                    EnableEvents(eventSource, EventLevel.LogAlways, EventKeywords.All, new Dictionary<string, string>() { { "Metrics", "Example\\upDownCounter;Example\\counter" } });
+                }
+            }
+        }
+    }
+}
